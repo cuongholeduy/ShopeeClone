bcf3a5e7e30143d145b09b3277becbd8507fb43c
Cuong Ho <cuong.holeduy@gmail.com>
2023-06-27 14:33:16 +0700

    custom class container

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,7 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
     "react/jsx-no-target-blank": "warn",
+    "@typescript-eslint/no-var-requires": 0,
     // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
     "prettier/prettier": [
       "warn",
