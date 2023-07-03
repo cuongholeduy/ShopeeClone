0511282d77d6e11243787593f5a188dd97e8d0f0
Cuong Ho <cuong.holeduy@gmail.com>
2023-07-03 21:52:10 +0700

    update useRouteElements file

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { Navigate, Outlet, useRoutes } from "react-router-dom"
 
 import { AppContext } from "./contexts/app.context"
+import path from "./constants/path"
 
 import ProductList from "./pages/ProductList"
 import Login from "./pages/Login"
@@ -12,12 +13,12 @@ import Profile from "./pages/Profile"
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+  return isAuthenticated ? <Outlet /> : <Navigate to={path.login} />
 }
 
 function RejectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
-  return !isAuthenticated ? <Outlet /> : <Navigate to="/" />
+  return !isAuthenticated ? <Outlet /> : <Navigate to={path.home} />
 }
 
 export default function useRouteElements() {
