507e1af7e65f0ca384aed95b63280d019d47b5d2
Cuong Ho <cuong.holeduy@gmail.com>
2023-07-03 14:26:53 +0700

    create popover component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,82 +1,51 @@
-import { useState, useRef } from "react"
 import { Link } from "react-router-dom"
-import { useFloating, FloatingPortal, arrow, shift, offset } from "@floating-ui/react"
-import { motion, AnimatePresence } from "framer-motion"
 import { GlobeAltIcon, ChevronDownIcon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/outline"
 
 import LogoIcon from "../LogoIcon"
+import Popover from "../Popover"
 
 export default function Header() {
-  const [open, setOpen] = useState(false)
-  const arrowRef = useRef<HTMLElement>(null)
-  const { x, y, refs, strategy, middlewareData } = useFloating({
-    middleware: [
-      offset(6),
-      shift(),
-      arrow({
-        element: arrowRef
-      })
-    ]
-  })
-
-  const showPopover = () => {
-    setOpen(true)
-  }
-
-  const hidePopover = () => {
-    setOpen(false)
-  }
-
   return (
     <div className="bg-gradient-to-b from-[#f53d2d] to-[#f63] pb-5 pt-2 text-white">
       <div className="container">
         <div className="flex justify-end">
-          <div
+          <Popover
             className="flex cursor-pointer items-center py-1 hover:text-gray-300"
-            ref={refs.setReference}
-            onMouseEnter={showPopover}
-            onMouseLeave={hidePopover}
+            renderPopover={
+              <div className="relative rounded-sm border border-gray-200 bg-white shadow-md">
+                <div className="flex flex-col py-2 pl-3 pr-28">
+                  <button className="px-3 py-2 text-left hover:text-orange">Tiếng Việt</button>
+                  <button className="px-3 py-2 text-left hover:text-orange">English</button>
+                </div>
+              </div>
+            }
           >
             <GlobeAltIcon className="h-5 w-5" />
             <span className="mx-1">Tiếng Việt</span>
             <ChevronDownIcon className="h-5 w-5" />
-            <FloatingPortal>
-              <AnimatePresence>
-                {open && (
-                  <motion.div
-                    ref={refs.setFloating}
-                    style={{
-                      position: strategy,
-                      top: y ?? 0,
-                      left: x ?? 0,
-                      width: "max-content",
-                      transformOrigin: `${middlewareData.arrow?.x}px top`
-                    }}
-                    initial={{ opacity: 0, transform: "scale(0)" }}
-                    animate={{ opacity: 1, transform: "scale(1)" }}
-                    exit={{ opacity: 0, transform: "scale(0)" }}
-                    transition={{ duration: 0.2 }}
-                  >
-                    <span
-                      ref={arrowRef}
-                      className="absolute z-10 translate-y-[-90%] border-[11px] border-x-transparent border-b-white border-t-transparent"
-                      style={{
-                        left: middlewareData.arrow?.x,
-                        top: middlewareData.arrow?.y
-                      }}
-                    />
-                    <div className="relative rounded-sm border border-gray-200 bg-white shadow-md">
-                      <div className="flex flex-col px-3 py-2">
-                        <button className="px-3 py-2 hover:text-orange">Tiếng Việt</button>
-                        <button className="px-3 py-2 hover:text-orange">English</button>
-                      </div>
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </FloatingPortal>
-          </div>
-          <div className="ml-6 flex cursor-pointer items-center py-1 hover:text-gray-300">
+          </Popover>
+          <Popover
+            className="ml-6 flex cursor-pointer items-center py-1 hover:text-gray-300"
+            renderPopover={
+              <div className="relative rounded-sm border border-gray-200 bg-white shadow-md">
+                <Link
+                  to="/"
+                  className="block w-full bg-white px-4 py-3 text-left hover:bg-slate-100 hover:text-cyan-500"
+                >
+                  Tài khoản của tôi
+                </Link>
+                <Link
+                  to="/"
+                  className="block w-full bg-white px-4 py-3 text-left hover:bg-slate-100 hover:text-cyan-500"
+                >
+                  Đơn mua
+                </Link>
+                <button className="block w-full bg-white px-4 py-3 text-left hover:bg-slate-100 hover:text-cyan-500">
+                  Đăng xuất
+                </button>
+              </div>
+            }
+          >
             <div className="mr-2 h-6 w-6 flex-shrink-0">
               <img
                 src="https://picsum.photos/200/300"
@@ -85,7 +54,7 @@ export default function Header() {
               />
             </div>
             <div>Cuong Ho</div>
-          </div>
+          </Popover>
         </div>
         <div className="mt-4 grid grid-cols-12 items-center justify-items-center gap-4">
           <Link to="/" className="col-span-2">
@@ -105,9 +74,66 @@ export default function Header() {
             </div>
           </form>
           <div className="col-span-1">
-            <Link to="/">
-              <ShoppingCartIcon className="h-8 w-8" />
-            </Link>
+            <Popover
+              renderPopover={
+                <div className="relative max-w-[400px] rounded-sm border border-gray-200 bg-white text-sm shadow-md">
+                  <div className="p-2">
+                    <div className="capitalize text-gray-400">Sản phẩm mới thêm</div>
+                    <div className="mt-5">
+                      <div className="mt-4 flex">
+                        <div className="flex-shrink-0">
+                          <img src="https://picsum.photos/200/300" alt="product" className="h-11 w-11 object-cover" />
+                        </div>
+                        <div className="ml-2 flex-grow overflow-hidden">
+                          <div className="truncate">
+                            [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24cm
+                          </div>
+                        </div>
+                        <div className="ml-2 flex-shrink-0">
+                          <span className="text-orange">₫469.000</span>
+                        </div>
+                      </div>
+                      <div className="mt-4 flex">
+                        <div className="flex-shrink-0">
+                          <img src="https://picsum.photos/200/300" alt="product" className="h-11 w-11 object-cover" />
+                        </div>
+                        <div className="ml-2 flex-grow overflow-hidden">
+                          <div className="truncate">
+                            [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24cm
+                          </div>
+                        </div>
+                        <div className="ml-2 flex-shrink-0">
+                          <span className="text-orange">₫469.000</span>
+                        </div>
+                      </div>
+                      <div className="mt-4 flex">
+                        <div className="flex-shrink-0">
+                          <img src="https://picsum.photos/200/300" alt="product" className="h-11 w-11 object-cover" />
+                        </div>
+                        <div className="ml-2 flex-grow overflow-hidden">
+                          <div className="truncate">
+                            [LIFEMCMBP2 -12% đơn 250K] Bộ Nồi Inox 3 Đáy SUNHOUSE SH334 16, 20, 24cm
+                          </div>
+                        </div>
+                        <div className="ml-2 flex-shrink-0">
+                          <span className="text-orange">₫469.000</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-6 flex items-center justify-between">
+                      <div className="text-xs capitalize text-gray-400">Thêm hàng vào giỏ</div>
+                      <button className="rounded-sm bg-orange px-4 py-2 capitalize text-white hover:bg-opacity-80">
+                        Xem giỏ hàng
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              }
+            >
+              <Link to="/">
+                <ShoppingCartIcon className="h-8 w-8" />
+              </Link>
+            </Popover>
           </div>
         </div>
       </div>
