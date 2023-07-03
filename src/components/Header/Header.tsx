04031f0109f62e7163958f1ade4f35d32e293c58
Cuong Ho <cuong.holeduy@gmail.com>
2023-07-03 08:32:56 +0700

    implement popover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,80 @@
+import { useState, useRef } from "react"
 import { Link } from "react-router-dom"
+import { useFloating, FloatingPortal, arrow, shift, offset } from "@floating-ui/react"
+import { motion, AnimatePresence } from "framer-motion"
 import { GlobeAltIcon, ChevronDownIcon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/outline"
 
 import LogoIcon from "../LogoIcon"
 
 export default function Header() {
+  const [open, setOpen] = useState(false)
+  const arrowRef = useRef<HTMLElement>(null)
+  const { x, y, refs, strategy, middlewareData } = useFloating({
+    middleware: [
+      offset(6),
+      shift(),
+      arrow({
+        element: arrowRef
+      })
+    ]
+  })
+
+  const showPopover = () => {
+    setOpen(true)
+  }
+
+  const hidePopover = () => {
+    setOpen(false)
+  }
+
   return (
     <div className="bg-gradient-to-b from-[#f53d2d] to-[#f63] pb-5 pt-2 text-white">
       <div className="container">
         <div className="flex justify-end">
-          <div className="flex cursor-pointer items-center py-1 hover:text-gray-300">
+          <div
+            className="flex cursor-pointer items-center py-1 hover:text-gray-300"
+            ref={refs.setReference}
+            onMouseEnter={showPopover}
+            onMouseLeave={hidePopover}
+          >
             <GlobeAltIcon className="h-5 w-5" />
             <span className="mx-1">Tiếng Việt</span>
             <ChevronDownIcon className="h-5 w-5" />
+            <FloatingPortal>
+              <AnimatePresence>
+                {open && (
+                  <motion.div
+                    ref={refs.setFloating}
+                    style={{
+                      position: strategy,
+                      top: y ?? 0,
+                      left: x ?? 0,
+                      width: "max-content",
+                      transformOrigin: `${middlewareData.arrow?.x}px top`
+                    }}
+                    initial={{ opacity: 0, transform: "scale(0)" }}
+                    animate={{ opacity: 1, transform: "scale(1)" }}
+                    exit={{ opacity: 0, transform: "scale(0)" }}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <span
+                      ref={arrowRef}
+                      className="absolute z-10 translate-y-[-90%] border-[11px] border-x-transparent border-b-white border-t-transparent"
+                      style={{
+                        left: middlewareData.arrow?.x,
+                        top: middlewareData.arrow?.y
+                      }}
+                    />
+                    <div className="relative rounded-sm border border-gray-200 bg-white shadow-md">
+                      <div className="flex flex-col px-3 py-2">
+                        <button className="px-3 py-2 hover:text-orange">Tiếng Việt</button>
+                        <button className="px-3 py-2 hover:text-orange">English</button>
+                      </div>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </FloatingPortal>
           </div>
           <div className="ml-6 flex cursor-pointer items-center py-1 hover:text-gray-300">
             <div className="mr-2 h-6 w-6 flex-shrink-0">
