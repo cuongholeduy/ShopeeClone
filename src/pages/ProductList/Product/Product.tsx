7fdaac84f73e7a6d48937f7cebd1eb0a84d4ccc0
Cuong Ho <cuong.holeduy@gmail.com>
2023-07-07 08:51:11 +0700

    code UI product

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,3 +1,50 @@
+import { Link } from "react-router-dom"
+import { StarIcon } from "@heroicons/react/24/solid"
+
+import path from "src/constants/path"
+
 export default function Product() {
-  return <div>Product</div>
+  return (
+    <Link to={path.home}>
+      <div className="overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.25rem] hover:shadow-md">
+        <div className="relative w-full pt-[100%]">
+          <img
+            src="https://picsum.photos/200/300"
+            alt="product"
+            className="absolute left-0 top-0 h-full w-full bg-white object-cover"
+          />
+        </div>
+        <div className="overflow-hidden p-2">
+          <div className="line-clamp-2 min-h-[2rem] text-xs">
+            [HÀNG HIỆU] Thắt Lưng Da Nam Khoá Tự Động Cao Cấp Dây Nịt Năm Mắt Xoay Chính Hãng , Phong Cách Hàn Quốc -
+            v77men
+          </div>
+          <div className="mt-3 flex items-center">
+            <div className="max-w-[50%] truncate text-gray-500 line-through">
+              <span className="text-xs">₫</span>
+              <span>5.000</span>
+            </div>
+            <div className="ml-1 truncate text-orange">
+              <span className="text-xs">₫</span>
+              <span>2.000</span>
+            </div>
+          </div>
+          <div className="mt-3 flex items-center justify-start">
+            <div className="flex items-center">
+              <div className="relative">
+                <div className="absolute left-0 top-0 h-full overflow-hidden" style={{ width: "40%" }}>
+                  <StarIcon className="h-3 w-3 text-yellow-400" />
+                </div>
+                <StarIcon className="h-3 w-3 text-gray-400" />
+              </div>
+            </div>
+            <div className="ml-2 text-xs">
+              <span>Đã bán</span>
+              <span className="ml-1">5.67k</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -1,3 +1,46 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
+
 export default function SortProductList() {
-  return <div>SortProductList</div>
+  return (
+    <div className="bg-gray-300/40 px-3 py-4">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <div className="flex flex-wrap items-center gap-2">
+          <div>Sắp xếp theo</div>
+          <button className="h-8 bg-orange px-4 text-center text-sm capitalize text-white hover:bg-orange/80">
+            Phổ biến
+          </button>
+          <button className="h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100">
+            Mới nhất
+          </button>
+          <button className="h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100">
+            Bán chạy
+          </button>
+          <select
+            className="h-8 bg-white px-4 text-left text-sm capitalize text-black outline-none hover:bg-slate-100"
+            value=""
+          >
+            <option value="" disabled>
+              Giá
+            </option>
+            <option value="price:asc">Giá thấp đến cao</option>
+            <option value="price:desc">Giá cao đến thấp</option>
+          </select>
+        </div>
+        <div className="flex items-center">
+          <div>
+            <span className="text-orange">1</span>
+            <span>/2</span>
+          </div>
+          <div className="ml-2">
+            <button className="h-8 cursor-not-allowed rounded-bl-sm rounded-tl-sm bg-white/60 px-3 shadow hover:bg-slate-300">
+              <ChevronLeftIcon className="h-3 w-3" />
+            </button>
+            <button className="h-8 rounded-br-sm rounded-tr-sm bg-white px-3 shadow hover:bg-slate-300">
+              <ChevronRightIcon className="h-3 w-3" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
